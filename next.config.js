a9c8da79611dc1b1d62f714cf38daa4980520480
Beto de Castro Moreira <humberto.mn@gmail.com>
2026-03-02 12:38:01 +1000

    feat: add /beto-admin rewrite and redirect /admin to /beto-admin

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ import './src/env.js'
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/beto-admin/:path*',
+        destination: '/admin/:path*',
+      },
+    ]
+  },
 }
 
 export default config
