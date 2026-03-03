ccd2b483a0cc73287a9776b0b328607754ca364d
Beto de Castro Moreira <humberto.mn@gmail.com>
2026-03-03 20:25:41 +1000

    fix: remove sticky positioning from navbar

--- src/components/public/sidebar-layout.tsx
+++ src/components/public/sidebar-layout.tsx
@@ -28,7 +28,7 @@ export function SidebarLayout({ groups, section, children }: SidebarLayoutProps)
       </div>
       <div className="flex gap-8">
         <aside className="hidden w-56 shrink-0 md:block">
-          <div className="sticky top-[4.5rem]">
+          <div className="sticky top-4">
             <CategoryTreeSidebar groups={groups} section={section} />
           </div>
         </aside>
