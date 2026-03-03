bec068cdcf705e751094513c28643b4e1fe3070c
Beto de Castro Moreira <humberto.mn@gmail.com>
2026-03-03 11:19:02 +1000

    feat: use animated shifting gradient on button to match logo

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-gradient-to-r from-sky-300 via-violet-300 to-emerald-300 text-primary-foreground hover:opacity-90',
+          'btn-gradient text-primary-foreground hover:opacity-90',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
