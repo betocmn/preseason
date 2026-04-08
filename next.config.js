165fc428a9e689072e3696acdc7103c8247f951f
Beto de Castro Moreira <humberto.mn@gmail.com>
2026-04-08 15:06:07 -0300

    fix: remove global html bot override

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ import './src/env.js'
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  // Force metadata into <head> for all user agents so link unfurlers always see OG tags.
-  htmlLimitedBots: /.*/,
   images: {
     formats: ['image/avif', 'image/webp'],
     imageSizes: [16, 32, 48, 64, 96, 128],
